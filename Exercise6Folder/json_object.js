cfefb4e9335825c0af4072e6af38b90334871138
JamesBain999 <baino999@gmail.com>
2023-12-08 14:19:00 +0800

    Finished Exercise 6

---
+++ Exercise6Folder/json_object.js
@@ -0,0 +1,16 @@
+var book = {
+    title: "Moby Dick",
+    description: "Whale book",
+    author: "Herman Melville",
+    pages: 427
+}
+
+console.log(book.title)
+console.log(book.description)
+console.log(book.author)
+console.log(book.pages)
+console.log(book)
+
+book.description = "Captain Ahab"
+
+console.log(book.description)
