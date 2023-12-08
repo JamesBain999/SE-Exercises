88b28b04845b0b45062f006e4053442c50f025d7
JamesBain999 <baino999@gmail.com>
2023-12-08 14:08:21 +0800

    Finished Exercise 5

---
+++ Exercise5Folder/Array.js
@@ -0,0 +1,19 @@
+var jamesArray = [1,2,3,4,5];
+
+console.log(jamesArray);
+
+jamesArray[1] = "two";
+
+console.log(jamesArray);
+
+jamesArray[4] = "five";
+
+console.log(jamesArray);
+
+jamesArray.unshift(0)
+
+console.log(jamesArray);
+
+jamesArray.pop();
+
+console.log(jamesArray);
