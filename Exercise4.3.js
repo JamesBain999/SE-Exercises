20381eab9575ebaacdd41451d14d4e74bcc39839
JamesBain999 <baino999@gmail.com>
2023-12-08 11:04:21 +0800

    Added Folders

--- Exercise4.3.js
+++
@@ -1,9 +0,0 @@
-function pairOfNumbers(a,b) {
-    if (a === 50 || b === 50 || a+b===50) {
-        return true
-    } else {
-        return false
-    }}
-    console.log(pairOfNumbers(50,2))
-    console.log(pairOfNumbers(6,2))
-    console.log(pairOfNumbers(24,26))
---
+++ Exercise4Folder/Exercise4.3.js
@@ -0,0 +1,9 @@
+function pairOfNumbers(a,b) {
+    if (a === 50 || b === 50 || a+b===50) {
+        return true
+    } else {
+        return false
+    }}
+    console.log(pairOfNumbers(50,2))
+    console.log(pairOfNumbers(6,2))
+    console.log(pairOfNumbers(24,26))
