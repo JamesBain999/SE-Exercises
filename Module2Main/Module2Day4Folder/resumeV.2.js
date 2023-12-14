0e4744d0c7ac9869c18df85320ca725d9000f67e
JamesBain999 <baino999@gmail.com>
2023-12-14 16:32:49 +0800

    Added the content of the website and added some javascript for transitions

--- Module2Main/Module2Day4Folder/resumeV.2.js
+++ Module2Main/Module2Day4Folder/resumeV.2.js
@@ -1,8 +1,20 @@
 let toggleAlignItems = true;
 
 function moveHeader() {
-    var content = document.getElementById('content');
+    var content = document.getElementById('hiddenContent');
     content.classList.toggle('hidden');
     var mainHeader = document.getElementById("mainHeader");
     mainHeader.style.maxHeight = (mainHeader.style.maxHeight === '200px') ? '100vh' : '200px';
 }
+
+function showSection(sectionId) {
+    const sections = document.querySelectorAll('.content');
+    sections.forEach(section => {
+      section.style.display = 'none';
+    });
+
+    const selectedSection = document.getElementById(sectionId);
+    if (selectedSection) {
+      selectedSection.style.display = 'block';
+    }
+  }
